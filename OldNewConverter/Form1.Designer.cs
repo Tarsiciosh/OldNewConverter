f84b28d358935778a63cb098161ae31d532943bd
SPA3CAP <tarsicio.spraggon@boschrexroth.com.ar>
2021-01-25 16:40:21 -0300

    this is a commit

--- OldNewConverter/Form1.Designer.cs
+++ OldNewConverter/Form1.Designer.cs
@@ -28,12 +28,104 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.readTxtFileButton = new System.Windows.Forms.Button();
+            this.configPathTextBox = new System.Windows.Forms.TextBox();
+            this.readExcelFileButton = new System.Windows.Forms.Button();
+            this.originFolderLabel = new System.Windows.Forms.Label();
+            this.originFolderTextBox = new System.Windows.Forms.TextBox();
+            this.destinationFolderTextBox = new System.Windows.Forms.TextBox();
+            this.destinationFolderLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // readTxtFileButton
+            // 
+            this.readTxtFileButton.Location = new System.Drawing.Point(413, 86);
+            this.readTxtFileButton.Name = "readTxtFileButton";
+            this.readTxtFileButton.Size = new System.Drawing.Size(75, 23);
+            this.readTxtFileButton.TabIndex = 0;
+            this.readTxtFileButton.Text = "Read txt";
+            this.readTxtFileButton.UseVisualStyleBackColor = true;
+            this.readTxtFileButton.Click += new System.EventHandler(this.readTxtFileButton_Click);
+            // 
+            // configPathTextBox
+            // 
+            this.configPathTextBox.Location = new System.Drawing.Point(12, 12);
+            this.configPathTextBox.Name = "configPathTextBox";
+            this.configPathTextBox.Size = new System.Drawing.Size(224, 20);
+            this.configPathTextBox.TabIndex = 1;
+            this.configPathTextBox.Text = "C:\\OldNewGateway\\config\\stations.xlsx";
+            // 
+            // readExcelFileButton
+            // 
+            this.readExcelFileButton.AllowDrop = true;
+            this.readExcelFileButton.Location = new System.Drawing.Point(242, 10);
+            this.readExcelFileButton.Name = "readExcelFileButton";
+            this.readExcelFileButton.Size = new System.Drawing.Size(75, 23);
+            this.readExcelFileButton.TabIndex = 2;
+            this.readExcelFileButton.Text = "Read Excel File";
+            this.readExcelFileButton.UseVisualStyleBackColor = true;
+            this.readExcelFileButton.Click += new System.EventHandler(this.readExcelFileButton_Click);
+            // 
+            // originFolderLabel
+            // 
+            this.originFolderLabel.AutoSize = true;
+            this.originFolderLabel.Location = new System.Drawing.Point(39, 54);
+            this.originFolderLabel.Name = "originFolderLabel";
+            this.originFolderLabel.Size = new System.Drawing.Size(100, 13);
+            this.originFolderLabel.TabIndex = 3;
+            this.originFolderLabel.Text = "Origin Folder Name:";
+            // 
+            // originFolderTextBox
+            // 
+            this.originFolderTextBox.Location = new System.Drawing.Point(145, 51);
+            this.originFolderTextBox.Name = "originFolderTextBox";
+            this.originFolderTextBox.Size = new System.Drawing.Size(229, 20);
+            this.originFolderTextBox.TabIndex = 4;
+            // 
+            // destinationFolderTextBox
+            // 
+            this.destinationFolderTextBox.Location = new System.Drawing.Point(145, 78);
+            this.destinationFolderTextBox.Name = "destinationFolderTextBox";
+            this.destinationFolderTextBox.Size = new System.Drawing.Size(229, 20);
+            this.destinationFolderTextBox.TabIndex = 6;
+            // 
+            // destinationFolderLabel
+            // 
+            this.destinationFolderLabel.AutoSize = true;
+            this.destinationFolderLabel.Location = new System.Drawing.Point(13, 81);
+            this.destinationFolderLabel.Name = "destinationFolderLabel";
+            this.destinationFolderLabel.Size = new System.Drawing.Size(126, 13);
+            this.destinationFolderLabel.TabIndex = 5;
+            this.destinationFolderLabel.Text = "Destination Folder Name:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(500, 121);
+            this.Controls.Add(this.destinationFolderTextBox);
+            this.Controls.Add(this.destinationFolderLabel);
+            this.Controls.Add(this.originFolderTextBox);
+            this.Controls.Add(this.originFolderLabel);
+            this.Controls.Add(this.readExcelFileButton);
+            this.Controls.Add(this.configPathTextBox);
+            this.Controls.Add(this.readTxtFileButton);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button readTxtFileButton;
+        private System.Windows.Forms.TextBox configPathTextBox;
+        private System.Windows.Forms.Button readExcelFileButton;
+        private System.Windows.Forms.Label originFolderLabel;
+        private System.Windows.Forms.TextBox originFolderTextBox;
+        private System.Windows.Forms.TextBox destinationFolderTextBox;
+        private System.Windows.Forms.Label destinationFolderLabel;
     }
 }
 
--- OldNewConverter/Form1.cs
+++ OldNewConverter/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
 
 namespace OldNewConverter
 {
@@ -16,5 +18,145 @@ namespace OldNewConverter
         {
             InitializeComponent();
         }
+
+        private void readTxtFileButton_Click(object sender, EventArgs e)
+        {
+            string originFolderString = originFolderTextBox.Text;
+            string destinationFolderString = destinationFolderTextBox.Text;
+            string destinationFilePath;
+
+            System.IO.StreamReader originStreamReader;
+            System.IO.StreamWriter destinationStreamWriter;
+
+            char[] originBuffer = new char[1024];
+
+            IEnumerable<string> filePaths = System.IO.Directory.EnumerateFiles(originFolderString, "*.json", System.IO.SearchOption.AllDirectories);
+            foreach (string originFilePath in filePaths) //all the .txt files in that folder and subfolders
+            {
+                int index;
+                string torqueString, angleString;
+
+                // open and read data from the origin file
+                originStreamReader = System.IO.File.OpenText(originFilePath); //open file
+                originStreamReader.Read(originBuffer, 0, originBuffer.Length - 1); // read file
+                string originFileString = new string(originBuffer); // convert to string 
+
+                index = originFileString.IndexOf("tightening steps");
+                index = originFileString.IndexOf("torque", index);
+                torqueString = new String(originBuffer, index + 9, 8);
+
+                index = originFileString.IndexOf("tightening steps");
+                index = originFileString.IndexOf("angle", index);
+                angleString = new String(originBuffer, index + 8, 10);
+
+                MessageBox.Show(torqueString);
+                MessageBox.Show(angleString);
+
+                // Writes data to the destination file
+                destinationFilePath = System.IO.Path.Combine(destinationFolderString, "test.txt");
+                destinationStreamWriter = System.IO.File.CreateText(destinationFilePath);
+
+                destinationStreamWriter.Write("torque= " + torqueString + "angle= " + angleString);
+
+                originStreamReader.Close();
+                destinationStreamWriter.Close();
+
+                System.IO.File.Delete(originFilePath);
+            }
+        }
+
+        private void readExcelFileButton_Click(object sender, EventArgs e)
+        {
+            Excel.Application oXL;
+            Excel._Workbook oWB;
+            Excel._Worksheet oSheet;
+            //Excel.Range oRng;
+
+            try
+            {
+                // to read the path of the file from the text box
+                string path = configPathTextBox.Text;
+                path = path.Replace(@"\", @"\\");
+
+                // to hardcode the path to a string
+                path = "C:\\OldNewGateway\\config\\stations.xlsx";
+
+                //start excel and get application object.
+                oXL = new Excel.Application();
+                oXL.Visible = true;
+
+                //open a workbook.
+                oWB = (Excel._Workbook)(oXL.Workbooks.Open(@path));
+                oSheet = (Excel._Worksheet)oWB.ActiveSheet; // .Sheets.Item()
+
+                oSheet.Cells[1, 2] = "Station Name";
+
+                //originFolderTextBox.Text = oSheet.get_Range("C2", "C2").Value2;
+                //destinationFolderTextBox.Text = oSheet.get_Range("D2", "D2").Value2;
+
+                originFolderTextBox.Text = oSheet.Cells[2,3].Value2; // then change the 2 with a variable to iterate al the file
+                destinationFolderTextBox.Text = oSheet.Cells[2,4].Value2;
+
+                oXL.Visible = false;
+                oXL.UserControl = false;
+
+                oWB.Save();
+                oWB.Close();
+
+            } catch (Exception theException) //catch and report the error if there is any
+            {
+                string errorMessage;
+                errorMessage = "Error:";
+                errorMessage = String.Concat( errorMessage, theException.Message);
+                errorMessage = String.Concat( errorMessage, "Line: ");
+                errorMessage = String.Concat(errorMessage, theException.Source);
+
+                MessageBox.Show(errorMessage, "Error");
+            }
+
+        }
     }
 }
+
+
+
+/*
+           string configPath = configPathTextBox.Text;  //string configPath = "C:\OldNewGateway\config";
+           string stationID, stationName, originPath, destinationPath;
+
+           int startIndex, endIndex;
+           string delimiter = "*";
+
+           char[] configBuffer = new char[1024];
+           System.IO.StreamReader configStreamReader;
+
+           configStreamReader = System.IO.File.OpenText(configPath);
+           configStreamReader.Read(configBuffer, 0, configBuffer.Length - 1);
+
+           string configString = new string(configBuffer);
+
+           endIndex = 0;
+
+
+           for (int i=0; i<3; i++) {
+               startIndex = endIndex;
+               endIndex = configString.IndexOf(delimiter, startIndex + 1); 
+               stationID = new string(configBuffer, startIndex + 1, endIndex - startIndex - 1); 
+               MessageBox.Show("station ID: " + stationID); 
+
+               startIndex = endIndex;
+               endIndex = configString.IndexOf(delimiter, startIndex + 1);
+               stationName = new string(configBuffer, startIndex + 1, endIndex - startIndex - 1); 
+               MessageBox.Show("name: " + stationName);
+
+               startIndex = endIndex;
+               endIndex = configString.IndexOf(delimiter, startIndex + 1);
+               originPath = new string(configBuffer, startIndex + 1, endIndex - startIndex - 1); 
+               MessageBox.Show("origin: " + originPath);
+
+               startIndex = endIndex;
+               endIndex = configString.IndexOf(delimiter, startIndex + 1);
+               destinationPath = new string(configBuffer, startIndex + 1, endIndex - startIndex - 1); 
+               MessageBox.Show("destination: " + destinationPath);
+           } 
+           */
